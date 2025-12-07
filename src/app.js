86295e3b0597f7c5fdd9ed78904a180fc71bc339
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2025-12-07 16:48:41 +0700

    Add rate limiting to API routes and middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middleware/errorHandler');
+const { generalLimiter } = require('./middleware/rateLimiter');
 
 const app = express();
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(generalLimiter);
 
 // Routes
 app.get('/', (req, res) => {
